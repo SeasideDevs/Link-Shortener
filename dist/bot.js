3083d4aa63aec5e750d1add23f5bc0cf39680cde
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-20 01:47:44 +0000

    Basic client done

--- dist/bot.js
+++ dist/bot.js
@@ -1,1 +1,23 @@
 "use strict";
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, "__esModule", { value: true });
+var discord_js_1 = __importDefault(require("discord.js"));
+var bot = new discord_js_1.default.Client({
+  disableMentions: "all",
+  allowedMentions: {
+    parse: ["users"],
+  },
+  presence: {
+    activity: {
+      name: "shortening links!",
+      type: "COMPETING",
+    },
+  },
+  ws: {
+    intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
+  },
+});
---
+++ src/bot.ts
@@ -0,0 +1,16 @@
+import Discord from "discord.js";
+const bot = new Discord.Client({
+  disableMentions: "all",
+  allowedMentions: {
+    parse: ["users"],
+  },
+  presence: {
+    activity: {
+      name: "shortening links!",
+      type: "COMPETING",
+    },
+  },
+  ws: {
+    intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
+  },
+});
