e9d71c61a75cd064009f504c493dfbebcb5fea89
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-20 01:28:54 +0000

    Add the sharding manager

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var discord_js_1 = require("discord.js");
+var logger_1 = require("./functions/logger");
+var manager = new discord_js_1.ShardingManager("bot.js", {
+  token: process.env.BOT_TOKEN,
+});
+manager.on("shardCreate", function (shard) {
+  logger_1.log("Shard #" + shard.id + " Started", "SHARD INFO");
+  shard.on("death", function () {
+    logger_1.log("Shard #{shard.id} died", "SHARD ERROR");
+  });
+  shard.on("disconnect", function () {
+    logger_1.log("Shard #" + shard.id + " was disconnected", "SHARD ERROR");
+  });
+  shard.on("reconnecting", function () {
+    logger_1.log(
+      "Shard #" + shard.id + " is attempting to reconnect",
+      "SHARD WARN"
+    );
+  });
+});
+manager.spawn();
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
 import { ShardingManager } from "discord.js";
 import { log } from "./functions/logger";
+const manager = new ShardingManager("bot.js", {
+  token: process.env.BOT_TOKEN,
+});
+
+manager.on("shardCreate", (shard) => {
+  log(`Shard #${shard.id} Started`, "SHARD INFO");
+  shard.on("death", () => {
+    log(`Shard #{shard.id} died`, "SHARD ERROR");
+  });
+  shard.on("disconnect", () => {
+    log(`Shard #${shard.id} was disconnected`, "SHARD ERROR");
+  });
+  shard.on("reconnecting", () => {
+    log(`Shard #${shard.id} is attempting to reconnect`, "SHARD WARN");
+  });
+});
+
+manager.spawn();
