19e596103c23e09f8325252afa1d8888929ebc39
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-20 02:04:52 +0000

    Start work on command handler

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,6 @@
 import Discord from "discord.js";
+import { log } from "./functions/logger";
+import { readdirSync } from "fs";
 const bot = new Discord.Client({
   disableMentions: "all",
   allowedMentions: {
@@ -13,4 +15,23 @@ const bot = new Discord.Client({
   ws: {
     intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
   },
+});
+
+const commandFiles = readdirSync("./commands");
+const commands = new Discord.Collection();
+
+bot.on("ready", async () => {
+  log(`Logged in as ${bot.user!.tag}`, "SUCCESS");
+});
+
+bot.on("guildCreate", async (guild) => {
+  log(`Joined ${guild.name} (${guild.id})`, "INFO");
+});
+
+bot.on("guildDelete", async (guild) => {
+  log(`Left ${guild.name} (${guild.id})`, "INFO");
+});
+
+bot.on("message", async (msg) => {
+  if (msg.author.bot) return;
 });
