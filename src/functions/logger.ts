791456478d933dc807e1668e00a9952da5c7b27d
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-21 21:13:49 +0000

    Go back to vanilla JS

--- src/functions/logger.ts
+++
@@ -1,9 +0,0 @@
-import chalk from "chalk";
-type BasicLogType = "SUCCESS" | "INFO" | "WARN" | "ERROR";
-type ShardLogType = `SHARD ${BasicLogType}`;
-type WebServerLogType = `WEB SERVER ${BasicLogType}`;
-type LogType = BasicLogType | ShardLogType | WebServerLogType;
-export function log(msg: string, type: LogType): TypeError | null {
-  console.log(chalk.greenBright(type), msg);
-  return null;
-}
