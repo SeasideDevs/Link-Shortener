6b360e71008436c95738214c250557cb5b75a5c8
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-23 01:57:24 +0000

    Done with config parser

--- src/functions/config.js
+++ src/functions/config.js
@@ -1,2 +1,10 @@
 const { parse } = require("@iarna/toml");
-parse();
+const { readFileSync, readdirSync } = require("fs");
+const parseConfig = () => {
+  const configFile = readFileSync(__dirname + "/../config.toml", {
+    encoding: "utf-8",
+  });
+  return parse(configFile);
+};
+
+module.exports = { parseConfig };
