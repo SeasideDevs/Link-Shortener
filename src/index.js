ff0bfc1e7f5168c10e12be5a7f923acefe1fe451
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-25 01:43:44 +0000

    Change shard disconnect to a warn instead of an error And update old log types to new log types

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ const manager = new ShardingManager(__dirname + "/bot.js", {
 });
 
 manager.on("shardCreate", (shard) => {
-  log(`Shard #${shard.id} Started`, "SHARD INFO");
+  log(`Shard #${shard.id} Started`, "shard_info");
   shard.on("death", () => {
-    log(`Shard #{shard.id} died`, "SHARD ERROR");
+    log(`Shard #{shard.id} died`, "shard_error");
   });
   shard.on("disconnect", () => {
-    log(`Shard #${shard.id} was disconnected`, "SHARD ERROR");
+    log(`Shard #${shard.id} was disconnected`, "shard_warning");
   });
   shard.on("reconnecting", () => {
-    log(`Shard #${shard.id} is attempting to reconnect`, "SHARD WARN");
+    log(`Shard #${shard.id} is attempting to reconnect`, "shard_warning");
   });
 });
 
