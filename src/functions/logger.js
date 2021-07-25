4e43c3ea3cba32cc3a5b27de88c94e131c71c294
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-25 02:53:13 +0000

    Start work on webhook logging

--- src/functions/logger.js
+++ src/functions/logger.js
@@ -1,4 +1,5 @@
 const chalk = require("chalk");
+const { fetch } = require("node-fetch");
 const { parseConfig } = require("./config");
 const config = parseConfig();
 module.exports = {
@@ -20,10 +21,27 @@ module.exports = {
       console.log(chalk.redBright("ERROR"), "Invalid Log Type");
       throw new TypeError("Invalid logger type");
     }
-    // This just makes the type more human friendly for local logs
-    const localHumanType = type.toUpperCase().replace("_", " ");
+    // If the log type is enabled for local logging then go ahead
     if (config.logging.local[type]) {
+      // This just makes the type more human friendly for local logs
+      const localHumanType = type.toUpperCase().replace("_", " ");
       console.log(chalk.hex(config.colors.logging[type])(localHumanType), msg);
+    }
+    // If the log type is enabled for webhook logging then go ahead
+    if (config.logging.webhook[type]) {
+      // TODO: Implement hex to integer so colors work
+      const body = {
+        embeds: [
+          {
+            title: "",
+          },
+        ],
+      };
+      // Send the data to the webhook
+      fetch(process.env.LOGGING_WEBHOOK, {
+        method: "POST",
+        body: JSON.stringify(body),
+      });
     }
   },
 };
