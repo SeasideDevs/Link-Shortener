cd45898c91b518f4e805b75dc800a61ca7073f37
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-26 02:09:42 +0000

    Finish webhook logging support

--- src/functions/logger.js
+++ src/functions/logger.js
@@ -1,5 +1,5 @@
 const chalk = require("chalk");
-const { fetch } = require("node-fetch");
+const fetch = require("node-fetch");
 const { parseConfig } = require("./config");
 const config = parseConfig();
 module.exports = {
@@ -29,18 +29,40 @@ module.exports = {
     }
     // If the log type is enabled for webhook logging then go ahead
     if (config.logging.webhook[type]) {
-      // TODO: Implement hex to integer so colors work
+      /*
+        Here we split the type into an array of items divided by a dash (-).
+        Then we create a array to hold items.
+        Now we make a loop and go through each item in the array of items split and uppercase the first letter.
+        Then we push it to the array previously created.
+        Lastly, we join the array with spaces and we got a human friendly log type!
+      */
+      const splitType = type.split("_");
+      const humanFriendlySplitType = [];
+      splitType.forEach((word) => {
+        const upperCased = word.replace(
+          word.charAt(0),
+          word.charAt(0).toUpperCase()
+        );
+        humanFriendlySplitType.push(upperCased);
+      });
+      const humanFriendlyType = humanFriendlySplitType.join(" ");
+      const color = parseInt(config.colors.logging[type].replace("#", ""), 16);
       const body = {
         embeds: [
           {
-            title: "",
+            color: color,
+            title: humanFriendlyType,
+            description: msg,
           },
         ],
       };
       // Send the data to the webhook
       fetch(process.env.LOGGING_WEBHOOK, {
         method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
+      }).then((res) => {
+        console.log(res);
       });
     }
   },
