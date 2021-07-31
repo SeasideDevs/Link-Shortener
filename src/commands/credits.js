20442f82e291a5e607d146fc43f53ff71a01b12c
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-31 00:35:02 -0500

    Credits command

--- src/commands/credits.js
+++ src/commands/credits.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: "ping",
-  aliases: ["latency"],
+  name: "credits",
+  aliases: ["credit"],
   access: {
     ownerOnly: false,
     nonOwnerAccessIDS: [],
@@ -8,41 +8,15 @@ module.exports = {
     guildMemberPermsRequired: [],
   },
   cooldowns: {
-    normal: "2",
-    reduced: "60s",
+    normal: "1",
+    reduced: "0.5",
   },
   async run(msg, bot, discord, config) {
-    // Send the embed
-    const message = await msg.channel.send(
+    msg.channel.send(
       new discord.MessageEmbed()
         .setColor(config.colors.main)
-        .setTitle("Pong?")
-        .setDescription("Awaiting Results")
-    );
-    // e
-    const ping = {
-      bot: Date.now() - message.createdAt,
-      api: bot.ws.ping,
-    };
-    if ((ping.bot > 500) | (ping.api > 250)) {
-      return config.colors.warn;
-    }
-    // Edit the embed with the ping
-    message.edit(
-      message.embeds[0]
-        .setTitle("Pong!")
-        .setColor(
-          (ping.bot > 500) | (ping.api > 250)
-            ? config.colors.warn
-            : config.colors.main
-        )
-        .setDescription(
-          `${
-            ping.bot > 500 ? config.emojis.warn : config.emojis.success
-          } **Latency:** ${ping.bot}ms\n${
-            ping.api > 250 ? config.emojis.warn : config.emojis.success
-          } **API Latency**: ${ping.api}ms`
-        )
+        .setTitle("Credits")
+        .setDescription("**X Daniel#0017** - Emote Designer")
     );
   },
 };
