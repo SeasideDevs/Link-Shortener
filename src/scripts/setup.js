dab707f4c1e56461296c2a1813dc666ccdfd0b66
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-08-02 00:37:55 -0500

    Test

--- src/scripts/setup.js
+++ src/scripts/setup.js
@@ -1,9 +1,1 @@
-const { prompt } = require("enquirer");
-(async () => {
-const response = await prompt({
-type: "input",
-name: "name",
-message: "What is your name?"
-})
-console.log(response)
-})()
+
